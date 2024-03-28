5503ff834568b2ec4e57969924d83b738ef0a416
Shuai99999 <gaoshuai099@gmail.com>
2024-03-28 14:51:47 +0800

    1

--- rrswlcmdb/views/re_chdata.py
+++ rrswlcmdb/views/re_chdata.py
@@ -153,8 +153,8 @@ def re_chdata(request):
                             code.write(sql_text)
 
     # subprocess.Popen(['su', '-', 'oracle', '/home/oracle/dba/prod/chdata.sh', db], stdout=subprocess.PIPE)
-    p = subprocess.Popen(['su', '-', 'oracle', '/home/oracle/dba/prod/chdata.sh', db], stdout=subprocess.PIPE)
+    p = subprocess.Popen(['su', '-', 'oracle', '/home/oracle/dba/prod/chdata.sh', db], stdout=subprocess.PIPE, encoding="utf-8")
     # p = subprocess.run(['su', '-', 'oracle', '/home/oracle/dba/prod/chdata.sh', db], capture_output = True, stdout=subprocess.PIPE).decode('utf-8')
-    chdata_result = p.stdout.encode
+    chdata_result = p.stdout
     return render(request, 're_chdata.html', {"chdata_result": chdata_result})
 
