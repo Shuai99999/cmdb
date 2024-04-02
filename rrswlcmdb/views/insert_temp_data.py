66df3d2fedf9486e3ffe092a5560b8dce3a3dd53
Shuai99999 <gaoshuai099@gmail.com>
2024-04-02 16:41:26 +0800

    1

--- rrswlcmdb/views/insert_temp_data.py
+++ rrswlcmdb/views/insert_temp_data.py
@@ -5,11 +5,13 @@ def insert_temp_data(request):
     if request.method == "GET":
         return render(request, "insert_temp_data.html")
 
-    if request.method=="POST":
+    if request.method == "POST":
         res = request.POST.get("temp_data", None)
         res = res.replace("\r\n", "\n")
-        # with open("/home/oracle/dba/bi/for_insert.sql", "w") as f:
-        #     f.write(str(res))
+        with open("/home/oracle/dba/bi/for_insert.sql", "w") as f:
+            f.seek(0)
+            f.truncate()
+            f.write(str(res))
         dbname = request.POST.get("dbname_choice", None)
         print(dbname)
     subprocess.Popen(['su', '-', 'oracle', '/home/oracle/dba/bi/tmp_' + dbname + '_data.sh'], stdout=subprocess.PIPE)
