70e1b10dc28ecc6f151fae46c4493c26166b5217
Shuai99999 <gaoshuai099@gmail.com>
2024-07-12 15:20:38 +0800

    1

--- rrswlcmdb/views/add_expdata_job.py
+++ rrswlcmdb/views/add_expdata_job.py
@@ -17,8 +17,10 @@ def add_expdata_job(request):
         filename = request.POST.get("filename", None)
         expdata_db = request.POST.get("expdata_db", None)
         expdata_sql = request.POST.get("expdata_sql", None)
-        print(expdata_sql)
         expdata_sql = expdata_sql.replace("\r\n", "\n")
+        print(expdata_db)
+        print(filename)
+        print(expdata_sql)
         with open("/home/mysql/dba/bi/auto_export/" + filename + "_input", "w") as f:
             f.write(str(expdata_sql))
             # f.write(str("\n"))
