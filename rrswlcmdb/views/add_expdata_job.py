3e3d3aff4d3567db3f3a02cf7aaea2cfe3b29b80
Shuai99999 <gaoshuai099@gmail.com>
2024-07-12 15:39:40 +0800

    1

--- rrswlcmdb/views/add_expdata_job.py
+++ rrswlcmdb/views/add_expdata_job.py
@@ -18,7 +18,7 @@ def add_expdata_job(request):
         expdata_db = request.POST.get("expdata_db", None)
         expdata_sql = request.POST.get("expdata_sql", None)
         expdata_sql = expdata_sql.replace("\r\n", "\n")
-        with open("/home/mysql/dba/bi/auto_export/" + filename + "_input", "w") as f:
+        with open("/home/mysql/dba/bi/auto_export/" + filename + "_inputs", "w+") as f:
             f.write(str(expdata_sql))
             # f.write(str("\n"))
     subprocess.Popen(['su', '-', 'mysql', '/home/mysql/dba/bi/auto_export/add_expdata_job.sh', expdata_db, filename], stdout=subprocess.PIPE)
