ed0e0e236978887f369a3d346e95b798d4679fb4
gaoshuai <gaoshuai099@gmail.com>
2024-07-22 10:30:35 +0800

    1

--- rrswlcmdb/views/download_file.py
+++ rrswlcmdb/views/download_file.py
@@ -6,10 +6,13 @@ from django.core.files.storage import FileSystemStorage
 import os
 
 def download_files(request):
-    files = os.listdir('media/uploads/')
-    response = HttpResponse(content_type='application/zip')
-    zip_file = FileSystemStorage().zip_folder('media/uploads/', files)
-    response['Content-Disposition'] = 'attachment; filename="files.zip"'
-    response['Content-Length'] = os.path.getsize(zip_file)
-    response.write(open(zip_file, 'rb').read())
-    return response
+    if request.method=="GET":
+        return render(request, "download_file.html")
+    else:
+        files = os.listdir('media/uploads/')
+        response = HttpResponse(content_type='application/zip')
+        zip_file = FileSystemStorage().zip_folder('media/uploads/', files)
+        response['Content-Disposition'] = 'attachment; filename="files.zip"'
+        response['Content-Length'] = os.path.getsize(zip_file)
+        response.write(open(zip_file, 'rb').read())
+        return redirect('/download_file/')
