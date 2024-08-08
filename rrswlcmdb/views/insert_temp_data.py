716284aa80290386fe8b1274b9d4cf0e14a01b53
gaoshuai <gaoshuai099@gmail.com>
2024-08-08 14:51:17 +0800

    1

--- rrswlcmdb/views/insert_temp_data.py
+++ rrswlcmdb/views/insert_temp_data.py
@@ -18,5 +18,5 @@ def insert_temp_data(request):
             f.write(str(res))
 
         # print(dbname)
-        subprocess.Popen(['su', '-', dbtype, '/home/' + dbtype + '/dba/bi/tmp_' + dbname + '_data.sh'], stdout=subprocess.PIPE)
+        subprocess.Popen(['nohup', 'su', '-', dbtype, '/home/' + dbtype + '/dba/bi/tmp_' + dbname + '_data.sh'], stdout=subprocess.PIPE)
     return render(request, 'insert_temp_data.html')
