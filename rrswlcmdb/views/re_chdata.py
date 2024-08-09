7458b54a13ff34524185bf43ec9825ef5f37f495
gaoshuai <gaoshuai099@gmail.com>
2024-08-09 14:51:57 +0800

    1

--- rrswlcmdb/views/re_chdata.py
+++ rrswlcmdb/views/re_chdata.py
@@ -229,6 +229,9 @@ def re_chdata(request):
     #     chdata_result = 'ERROR 1064 (42000) at line 1: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax'
     # return render(request, 're_chdata.html', {"chdata_result": chdata_result})
     cmd = 'nohup su - ' + db_type + ' /home/' + db_type + '/dba/prod/chdata.sh ' + db + ' > /tmp/re_chdata.log 2>&1'
-    chdata_result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+    # chdata_result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+    subprocess.run(cmd, shell=True)
+    chdata_result = subprocess.run('cat /tmp/re_chdata.log', shell=True, stdout=subprocess.PIPE)
 
+    # return render(request, 're_chdata.html', {"chdata_result": chdata_result.stdout.decode('utf-8')})
     return render(request, 're_chdata.html', {"chdata_result": chdata_result.stdout.decode('utf-8')})
