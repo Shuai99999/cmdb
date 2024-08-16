75e07fda6328b1ef6b61ff7238418093bbcab660
gaoshuai <gaoshuai099@gmail.com>
2024-08-16 11:00:20 +0800

    1

--- rrswlcmdb/views/ftpServer.py
+++ rrswlcmdb/views/ftpServer.py
@@ -1,25 +1,42 @@
 import os
+
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
-from pyftpdlib.servers import FTPServer, MultiprocessFTPServer, ThreadedFTPServer
-from django.core.management import BaseCommand
+from pyftpdlib.servers import FTPServer
 
 
-def ftpServer(self, *args, **options):
-    """
-    实现主要逻辑,启动ftp服务
-    :param args:
-    :param options:
-    :return:
-    """
+def main():
+    # Instantiate a dummy authorizer for managing 'virtual' users
     authorizer = DummyAuthorizer()
+
+    # Define a new user having full r/w permissions and a read-only
+    # anonymous user
+    # authorizer.add_user('user', '12345', '.', perm='elradfmwMT')
+    authorizer.add_anonymous('/data/ftp/export/dbExpfiles')
+
+    # Instantiate FTP handler class
     handler = FTPHandler
     handler.authorizer = authorizer
-    authorizer.add_anonymous('/data/ftp/export/dbExpfiles')
+
+    # Define a customized banner (string returned when client connects)
+    handler.banner = "pyftpdlib based ftpd ready."
+
+    # Specify a masquerade address and the range of ports to use for
+    # passive connections.  Decomment in case you're behind a NAT.
+    # handler.masquerade_address = '151.25.42.11'
+    # handler.passive_ports = range(60000, 65535)
+
+    # Instantiate FTP server class and listen on 0.0.0.0:2121
     address = ('', 2121)
-    server = ThreadedFTPServer(address, handler)
-    # server = ThreadedFTPServer((FTP_IP, FTP_POST), handler)
+    server = FTPServer(address, handler)
+
+    # set a limit for connections
+    server.max_cons = 256
+    server.max_cons_per_ip = 5
+
+    # start ftp server
     server.serve_forever()
 
+
 if __name__ == '__main__':
-    ftpServer()
+    main()
