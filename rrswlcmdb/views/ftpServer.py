a3d8d9b3d6898b8393acba203962f6247e0ce5e1
gaoshuai <gaoshuai099@gmail.com>
2024-08-16 10:33:45 +0800

    1

---
+++ rrswlcmdb/views/ftpServer.py
@@ -0,0 +1,25 @@
+import os
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer, MultiprocessFTPServer, ThreadedFTPServer
+from django.core.management import BaseCommand
+
+
+def ftpServer(self, *args, **options):
+    """
+    实现主要逻辑,启动ftp服务
+    :param args:
+    :param options:
+    :return:
+    """
+    authorizer = DummyAuthorizer()
+    handler = FTPHandler
+    handler.authorizer = authorizer
+    authorizer.add_anonymous('/data/ftp/export/dbExpfiles')
+    address = ('', 2121)
+    server = FTPServer(address, handler)
+    # server = ThreadedFTPServer((FTP_IP, FTP_POST), handler)
+    server.serve_forever()
+
+if __name__ == '__main__':
+    ftpServer()
